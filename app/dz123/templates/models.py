c39d8ed283ceb81cc99802c19b00eec2e4140e71
nleymiks <ovchinnikovalyybov@gmail.com>
2024-12-28 21:03:26 +0500

    123

---
+++ app/dz123/templates/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Reader(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
--- app/dz123/views.py
+++ app/dz123/views.py
@@ -1,26 +1,58 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
+from django.shortcuts import render, redirect
+from .models import Reader, Book
+from .forms import ReaderForm, BookForm
 
-import datetime
+def add_reader(request):
+    if request.method == "POST":
+        form = ReaderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('reader_list')
+    else:
+        form = ReaderForm()
+    return render(request, 'add_reader.html', {'form': form})
 
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
+def edit_reader(request, reader_id):
+    reader = get_object_or_404(Reader, id=reader_id)
+    if request.method == "POST":
+        form = ReaderForm(request.POST, instance=reader)
+        if form.is_valid():
+            form.save()
+            return redirect('reader_list')
+    else:
+        form = ReaderForm(instance=reader)
+    return render(request, 'edit_reader.html', {'form': form})
 
-def index(request):
-    template = loader.get_template("dz123/index.html")
-    context = {'title': 'Главная страница',
-               'pages': [('writers', 'Писатели'),
-                         ('books', 'Книги'),
-                         ]}
-    return HttpResponse(template.render(context, request))
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'edit_book.html', {'form': form})
+def delete_reader(request, reader_id):
+    reader = get_object_or_404(Reader, id=reader_id)
+    if request.method == "POST":
+        reader.delete()
+        return redirect('reader_list')
+    return render(request, 'confirm_delete.html', {'object': reader})
 
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'confirm_delete.html', {'object': book})
 
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse('<h1>Главная</h1><p>Добро пожаловать на главную страницу!</p>')
-
-def writers(request):
-    return HttpResponse('<h1>Писатели</h1><p>Здесь вы можете узнать о писателях.</p>')
-
-def books(request):
-    return HttpResponse('<h1>Топ лучших книг</h1><p>Здесь представлен топ лучших книг.</p>')
